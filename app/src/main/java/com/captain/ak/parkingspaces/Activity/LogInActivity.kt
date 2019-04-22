53b126d16c8c7487fef7c45dbb8f8e3211ec61ce
Ankit Kumar <nktkumar82@gmail.com>
2019-04-22 21:31:08 +0530

    Project added to firebase successfully and Login , Signup page created

---
+++ app/src/main/java/com/captain/ak/parkingspaces/Activity/LogInActivity.kt
@@ -0,0 +1,13 @@
+package com.captain.ak.parkingspaces.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.captain.ak.parkingspaces.R
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
