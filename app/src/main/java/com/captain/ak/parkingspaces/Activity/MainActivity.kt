65861df5d9ec92c9c70cd197dc701914f4c8325f
Ankit Kumar <nktkumar82@gmail.com>
2019-04-22 22:33:26 +0530

    login activity opening successfully with firebase authentication

--- app/src/main/java/com/captain/ak/parkingspaces/Activity/MainActivity.kt
+++ app/src/main/java/com/captain/ak/parkingspaces/Activity/MainActivity.kt
@@ -1,13 +1,47 @@
 package com.captain.ak.parkingspaces.Activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.captain.ak.parkingspaces.R
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+
+
+     lateinit var mAuth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //FirebaseApp.initializeApp(this)
+
+
+        mAuth = FirebaseAuth.getInstance()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser: FirebaseUser? = mAuth.currentUser
+
+        if (currentUser==null)
+        {
+            sendToStart()
+        }
+
+    }
+
+    private fun sendToStart() {
+
+        val intent = Intent(this,LogInActivity::class.java)
+
+        startActivity(intent)
+
+
     }
 }
