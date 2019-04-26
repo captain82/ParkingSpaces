5e29dfc9813ecd100fc5ffafd044909069bdb862
Ankit Kumar <nktkumar82@gmail.com>
2019-04-26 23:54:30 +0530

    Update activity problems fixed and now working properly and app completed

--- app/src/main/java/com/captain/ak/parkingspaces/Activity/AddPlacesActivity.kt
+++ app/src/main/java/com/captain/ak/parkingspaces/Activity/AddPlacesActivity.kt
@@ -2,6 +2,7 @@ package com.captain.ak.parkingspaces.Activity
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -13,7 +14,14 @@ import com.captain.ak.parkingspaces.databinding.ActivityAddPlacesBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import android.location.*
+import android.location.LocationListener
+import android.os.Looper
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
+import com.google.android.gms.location.*
+import java.lang.ClassCastException
 import java.util.*
+import java.util.jar.Manifest
 import kotlin.collections.HashMap
 
 
@@ -25,17 +33,36 @@ class AddPlacesActivity : AppCompatActivity() {
 
     lateinit var databaseRef:DatabaseReference
 
-    lateinit var locationManager:LocationManager
 
-    private var hasGps = false
 
-    private var hasNetwork = false
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    private  var locationGps:Location? = null
+    lateinit var loacationRequest: LocationRequest
 
-    private  var locationNetwork:Location? = null
+    lateinit var locationCallback: LocationCallback
 
-     var count:String = "0"
+    lateinit var location: Location
+
+      var count:String = "0"
+
+    val REQUEST_CODE = 1000
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode)
+        {
+            REQUEST_CODE->{
+                if (grantResults.size > 0)
+                {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                        Toast.makeText(this@AddPlacesActivity,"Permission Granted" , Toast.LENGTH_SHORT).show()
+                    else
+                        Toast.makeText(this@AddPlacesActivity,"Permission Denied" , Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +73,97 @@ class AddPlacesActivity : AppCompatActivity() {
 
         databaseRef = FirebaseDatabase.getInstance().getReference().child("Users")
 
-        binding.uploadBtn.setOnClickListener { upload() }
+        //************check Permission*************************
+
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission
+                .ACCESS_FINE_LOCATION))
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),REQUEST_CODE)
+        else
+        {
+            buildLocationRequest()
+            buildLocationCallback()
+
+            //Create FusedProviderClient
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+            if(ActivityCompat.checkSelfPermission(this@AddPlacesActivity,android.Manifest.permission
+                    .ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED   &&   ActivityCompat
+                    .checkSelfPermission(this@AddPlacesActivity,android.Manifest.permission
+                    .ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED )
+            {
+                ActivityCompat.requestPermissions(this@AddPlacesActivity, arrayOf(android.Manifest.permission
+                    .ACCESS_FINE_LOCATION),REQUEST_CODE)
+            }
+            fusedLocationProviderClient.requestLocationUpdates(loacationRequest,locationCallback, Looper.myLooper())
+        }
+
+        val currentUID = mAuth.currentUser!!.uid
+
+        databaseRef.child(currentUID).addValueEventListener(object:ValueEventListener{
+            override fun onCancelled(p0: DatabaseError?) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot?) {
+                val userModel = UserModel()
+                userModel.count = p0!!.getValue(UserModel::class.java)!!.count
+                Log.i("Count" , userModel.count.toString())
+                count = userModel.count
+            }
+
+
+        })
+
+        binding.uploadBtn.setOnClickListener {
+            upload()
+        }
+    }
+
+    private fun buildLocationCallback() {
+
+        locationCallback = object : LocationCallback(){
+
+            override fun onLocationResult(p0: LocationResult?) {
+                 location = p0!!.locations.get(p0!!.locations.size-1) //Get last Location
+                Log.i("lattitude" , location.latitude.toString()+"  "+location.longitude.toString())
+                binding.longitudeTxt.setText("Longitude : "+location.longitude.toString())
+                binding.lattitudeTxt.setText("Latitude : "+location.latitude.toString())
+
+                //********************For getting street address***********************************************************
+                val geocoder: Geocoder
+                val addresses: List<Address>
+                geocoder = Geocoder(this@AddPlacesActivity, Locale.getDefault())
+
+
+                addresses = geocoder.getFromLocation(
+                   location.latitude,
+                    location.longitude,
+                    1
+                ) // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+                val address =
+                    addresses[0].getAddressLine(0) // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                val city = addresses[0].getLocality()
+                val state = addresses[0].getAdminArea()
+                val country = addresses[0].getCountryName()
+                val postalCode = addresses[0].getPostalCode()
+                val knownName = addresses[0].getFeatureName()
+
+                Log.i("Add" , address+city+state+country+postalCode+knownName)
+                binding.streetEditText.setText(address+" "+city+" "+state)
+            }
+
+
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun buildLocationRequest() {
+        loacationRequest = LocationRequest()
+        loacationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        loacationRequest.interval = 5000
+        loacationRequest.fastestInterval=3000
+        loacationRequest.smallestDisplacement =     10f
     }
 
 
@@ -56,136 +173,8 @@ class AddPlacesActivity : AppCompatActivity() {
 
     }
 
-    @SuppressLint("MissingPermission")
     private fun upload() {
         val currentUID = mAuth.currentUser!!.uid
-//********************For getting Lattitiude and Longitude***************************************************************
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-
-        hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-
-        if(hasGps||hasNetwork) {
-            if (hasGps) {
-                Log.d("CodeAndroidLocation", "hasGps")
-                locationManager
-                    .requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, object : LocationListener {
-                        override fun onLocationChanged(location: Location?) {
-                            if (location != null) {
-                                locationGps = location
-                                binding.lattitudeTxt.setText("Lattitude : "+locationGps!!.latitude.toString())
-                                binding.longitudeTxt.setText("Longitude : "+locationGps!!.longitude.toString())
-
-                                Log.d("CodeAndroidLocation1" , "Network lattitude: "+locationGps.toString())
-                                Log.d("CodeAndroidLocation1" , "Network longitude: "+locationGps!!.longitude)
-                            }
-                        }
-
-                        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-                        }
-
-                        override fun onProviderEnabled(provider: String?) {
-                        }
-
-                        override fun onProviderDisabled(provider: String?) {
-                        }
-
-
-                    })
-
-                val localGpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                if (localGpsLocation != null) {
-                    locationGps = localGpsLocation
-                    Log.d("CodeAndroidLocation1" , "Network lattitude: "+locationGps!!.longitude.toString())
-
-                }
-            }
-
-            if (hasNetwork) {
-                Log.d("CodeAndroidLocation", "hasNetwork")
-                locationManager
-                    .requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0F, object : LocationListener {
-                        override fun onLocationChanged(location: Location?) {
-                            if (location != null) {
-                                locationNetwork = location
-                                binding.lattitudeTxt.setText("Lattitude : "+locationNetwork!!.latitude.toString())
-                                binding.longitudeTxt.setText("Longitude : "+locationNetwork!!.longitude.toString())
-                                Log.i("Location" , "Network lattitude: "+locationNetwork!!.latitude.toString())
-                                Log.i("Location" , "Network longitude: "+locationNetwork!!.longitude.toString())
-
-                            }
-                        }
-
-                        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-                        }
-
-                        override fun onProviderEnabled(provider: String?) {
-                        }
-
-                        override fun onProviderDisabled(provider: String?) {
-                        }
-
-
-                    })
-
-                val localNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-                if (localNetworkLocation != null) {
-                    locationNetwork = localNetworkLocation
-                }
-            }
-            if(locationGps!=null && locationNetwork!=null)
-            {
-                if(locationGps!!.accuracy>locationNetwork!!.accuracy)
-                {
-                    binding.lattitudeTxt.setText("Lattitude : "+locationNetwork!!.latitude.toString())
-                    binding.longitudeTxt.setText("Longitude : "+locationNetwork!!.longitude.toString())
-                    Log.d("CodeAndroidLocation" , "Network lattitude: "+locationNetwork!!.latitude)
-                    Log.d("CodeAndroidLocation" , "Network longitude: "+locationNetwork!!.longitude)
-
-                }
-                else
-                {
-                    binding.lattitudeTxt.setText("Lattitude : "+locationGps!!.latitude.toString())
-                    binding.longitudeTxt.setText("Longitude : "+locationGps!!.longitude.toString())
-
-                    Log.d("CodeAndroidLocation" , "Network lattitude: "+locationGps!!.latitude)
-                    Log.d("CodeAndroidLocation" , "Network longitude: "+locationGps!!.longitude)
-
-                }
-
-            }
-        }
-//*************************************End of getting Lattitude and Longitude*******************************8888888
-
-
-        //********************For getting street address***********************************************************
-        val geocoder: Geocoder
-        val addresses: List<Address>
-        geocoder = Geocoder(this, Locale.getDefault())
-
-
-        addresses = geocoder.getFromLocation(
-            locationGps!!.latitude,
-            locationGps!!.longitude,
-            1
-        ) // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-
-        val address =
-            addresses[0].getAddressLine(0) // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-        val city = addresses[0].getLocality()
-        val state = addresses[0].getAdminArea()
-        val country = addresses[0].getCountryName()
-        val postalCode = addresses[0].getPostalCode()
-        val knownName = addresses[0].getFeatureName()
-
-        Log.i("Add" , address+city+state+country+postalCode+knownName)
-        binding.streetEditText.setText(address+" "+city+" "+state)
-
-//*****************************************End of getting street Address****************************************************************8
-
-//***********************************For getting count**************************************************************
-
 
         databaseRef.child(currentUID).addValueEventListener(object:ValueEventListener{
             override fun onCancelled(p0: DatabaseError?) {
@@ -206,9 +195,9 @@ class AddPlacesActivity : AppCompatActivity() {
 
         var userMap = HashMap<String, String>()
 
-        userMap["lattitude"] = locationGps!!.latitude.toString()
+        userMap["lattitude"] = location.latitude.toString()
 
-        userMap["longitude"] = locationGps!!.longitude.toString()
+        userMap["longitude"] = location.longitude.toString()
 
         userMap["address"] = binding.streetEditText.text.toString()
 
@@ -221,6 +210,11 @@ class AddPlacesActivity : AppCompatActivity() {
         //*************for increasing the count**************
 
         databaseRef.child(currentUID).child("count").setValue((count.toInt()+1).toString())
+
+        Toast.makeText(this@AddPlacesActivity,"Place updated.",Toast.LENGTH_SHORT).show()
+
+        onBackPressed()
+
 
 //***********************************************************************************
 
