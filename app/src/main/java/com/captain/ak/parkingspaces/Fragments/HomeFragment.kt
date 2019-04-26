eb9e310a4f0fdd6cdd5220149ea0dfccf5098689
Ankit Kumar <nktkumar82@gmail.com>
2019-04-26 09:25:59 +0530

    Fonts changed and Home fragment updated

--- app/src/main/java/com/captain/ak/parkingspaces/Fragments/HomeFragment.kt
+++ app/src/main/java/com/captain/ak/parkingspaces/Fragments/HomeFragment.kt
@@ -27,6 +27,8 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
